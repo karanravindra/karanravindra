f1479ebde7ad54a495a9ff17db4d14b7c6cb26f5
Karan Ravindra <karan.ravindra1213@gmail.com>
2024-08-26 16:52:45 -0400

    sitemap and basic blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://karanravindra.me',
-  integrations: [sitemap()]
+  site: "https://karanravindra.me",
+  integrations: [sitemap()],
 });
