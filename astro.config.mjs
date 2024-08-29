ca74cde5d5a3647047fafebc4966c100df6171a4
Karan Ravindra <karan.ravindra1213@gmail.com>
2024-08-29 13:00:32 -0400

    downgrade tailwind + format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://karanravindra.me",
-  integrations: [sitemap()],
+  integrations: [sitemap(), tailwind(), icon()],
 });
