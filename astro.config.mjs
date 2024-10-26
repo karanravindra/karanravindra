56fadbe724eab3e1efe5db960b74f53de8d378f8
Karan Ravindra <karan.ravindra1213@gmail.com>
2024-10-26 14:19:48 -0400

    configure sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://karanravidra.me',
   integrations: [tailwind(), svelte(), sitemap()]
 });
