035110ed9b493119cc1d67d523f522f0fb73e2f7
Karan Ravindra <karan.ravindra1213@gmail.com>
2024-11-15 00:32:35 -0500

    switch to tailwind file

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,5 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://karanravindra.me",
-  integrations: [tailwind(), svelte(), sitemap()],
+  integrations: [tailwind({applyBaseStyles: false}), svelte(), sitemap()],
 });
